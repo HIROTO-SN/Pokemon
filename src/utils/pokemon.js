c841a5b01a8c56ff949491bf680d62b2609be4ec
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2023-11-02 08:18:48 +0900

    Add Loading description

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,9 +1,11 @@
 export const getAllPokemon = (url) => {
 	return new Promise((resolve, reject) => {
-		fetch(url)
-			.then((res) => res.json())
-			.then((data) => resolve(data))
-			.catch((reason) => console.error('アクセス拒否：', reason))
-			.finally(() => console.log('最終実行'))
+		setTimeout(() => {
+			fetch(url)
+				.then((res) => res.json())
+				.then((data) => resolve(data))
+				.catch((reason) => console.error('アクセス拒否：', reason))
+				.finally(() => console.log('最終実行'))
+		}, 2000);
 	});
 };
