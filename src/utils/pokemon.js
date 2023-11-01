7458a7bdba2b97af69c4599e26ce1a664473f5af
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2023-11-02 08:02:53 +0900

    Use promise

---
+++ src/utils/pokemon.js
@@ -0,0 +1,9 @@
+export const getAllPokemon = (url) => {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => resolve(data))
+			.catch((reason) => console.error('アクセス拒否：', reason))
+			.finally(() => console.log('最終実行'))
+	});
+};
