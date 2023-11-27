204706d6a4aceb4ff065cb07d219c33d06ccd77a
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2023-11-28 06:46:33 +0900

    toggle initial

--- pokedex/frontend/src/components/Search/Search.js
+++ pokedex/frontend/src/components/Search/Search.js
@@ -14,14 +14,24 @@ const Search = () => {
 							<input className="search-main-input" type="text" />
 							<input className="search-main-button" type="submit" id="search" />
 						</div>
-						<div className="search-exp">Search for a Pokemon by name or using its <br/> National Pokedex number.</div>
+						<div className="search-exp">
+							<span>
+								Search for a Pokemon by name or using its <br/> National Pokedex number.
+							</span>
+						</div>
 					<span className="search-main-comment">
 						Use the Advanced Search to explore Pokemon by type,<br/>weakness, Ability, and more!
 					</span>
 				</div>
 			</div>
-			<div className="filter-toggle-wrapper">
-
+			<div className="filter-toggle-wrapper"></div>
+			<div className="pokemon-filter-toggle">
+				<input id="chkFilter" type="checkbox" class="toggle-button"/>
+				<label class="filter-label" for="chkFilter">Show Advanced Search</label>
+				<div class="toggle-content">
+					<p>【不思議の国のアリスの内容が表示します。】<br/>
+					アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。「絵や会話のない本なんて、なんの役にもたたないじゃないの」とアリスは思いました。</p>
+			</div>
 			</div>
     </div>
   );
