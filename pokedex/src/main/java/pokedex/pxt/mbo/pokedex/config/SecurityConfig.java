a16bef98cd3430f4bf518711ff13cc21e6135822
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2024-01-03 17:08:52 +0900

    login config initial

---
+++ pokedex/src/main/java/pokedex/pxt/mbo/pokedex/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package pokedex.pxt.mbo.pokedex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// http.formLogin(login -> login
+		// 	.loginPage("/login")
+		// 	.loginProcessingUrl("/authenticate")
+		// 	.defaultSuccessUrl("/")
+		// 	.failureUrl("/login?error")
+		// 	.permitAll()
+		// ).logout(logout -> logout
+		// 	.logoutSuccessUrl("/")
+		// ).authorizeHttpRequests(authz -> authz
+		// 	.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+		// 	.requestMatchers("/").permitAll()
+		// 	.requestMatchers("/login").permitAll()
+		// 	.requestMatchers("/pokeList").permitAll()
+		// 	.requestMatchers("/api/**").authenticated()
+		// 	.anyRequest().denyAll()
+		// );
+		
+		http.csrf((csrf -> csrf.disable()))
+						.authorizeHttpRequests(authorize -> authorize
+							// authorize.anyRequest().authenticated())
+							.requestMatchers(HttpMethod.GET, "/pokeList").permitAll()
+							.requestMatchers(HttpMethod.GET, "/items").permitAll()
+							.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+							.anyRequest().authenticated()
+						).httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+	
+	@Bean
+	protected UserDetailsService users() {
+		UserDetails admin = User.builder()
+														.username("admin")
+														.password(passwordEncoder().encode("admin"))
+														.roles("ADMIN")
+														.build();
+		UserDetails user = User.builder()
+														.username("user")
+														.password(passwordEncoder().encode("user"))
+														.authorities("USER")
+														.build();
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	protected static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
