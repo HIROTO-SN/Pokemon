68a41d0e65d319941de12cdbaa3c0b65e5da5049
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2024-03-21 06:31:48 +0900

    Evolution next stage loop

--- pokedex/src/main/java/pokedex/pxt/mbo/pokedex/specification/EvolutionSpecification.java
+++ pokedex/src/main/java/pokedex/pxt/mbo/pokedex/specification/EvolutionSpecification.java
@@ -3,32 +3,29 @@ package pokedex.pxt.mbo.pokedex.specification;
 import org.springframework.data.jpa.domain.Specification;
 
 import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.JoinType;
-import jakarta.persistence.criteria.Root;
-import jakarta.persistence.criteria.Subquery;
+import jakarta.persistence.criteria.Predicate;
 import pokedex.pxt.mbo.pokedex.entity.pokemon.Pokemon;
 
 public class EvolutionSpecification<Evolution> {
 
 	/**
-	 * pokemonIdの属するGroupを全て（全ての進化種）を取得
+	 * 同じグループ（進化系）に属するポケモン進化系を取得
 	 * 
-	 * @param type Integer pokemonId
+	 * @param groupId int グループID
 	 * @return Specification<Evolution>
 	 */
-	// public Specification<Evolution> findGroupByPokemonId(int pokemonId) {
-	// 	return (root, query, builder) -> {
-	// 		Subquery<Integer> subquery = query.subquery(Integer.class);
-	// 		Root<Pokemon> evolutionRoot = subquery.from(Pokemon.class);
-	// 		Join<Evolution, Pokemon> pokemonJoin = evolutionRoot.join("pokemon", JoinType.LEFT);
-	// 		query.distinct(true);
-	// 		subquery.select(builder.evolutionRoot.get("groupdId"))
-	// 			.where(builder.equal(pokemonJoin.get("pokemonId"), pokemonId));
-
-	// 		return root.get("groupId").in(subquery);
-	// 	};
-	// }
+	public Specification<Evolution> groupIdEquals(int groupId) {
+		return (root, query, builder) -> {
+			Join<Evolution,Pokemon> pokemonJoin = root.join("pokemon", JoinType.LEFT);
+			
+			Predicate groupIdPredicate = builder.equal(root.get("groupId"), groupId);
+			query.distinct(true);
+			query.where(groupIdPredicate);
+			query.orderBy(builder.asc(root.get("stage")));
+			return pokemonJoin.get("pokemonId").in(2);
+		};
+	}
 
 	/**
 	 * pokemonIdに紐づく進化種情報を取得
